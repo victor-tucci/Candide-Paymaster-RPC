568d2a3b50a27fadf1e51ae52b2e28e3c435751f
victor-tucci <victor.tucci@beldex.io>
2024-10-30 10:17:16 +0530

    cors orgin issue fix

--- paymaster/settings.py
+++ paymaster/settings.py
@@ -34,9 +34,16 @@ ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', '10.0.2.2', 'localhost']
 
 # ALLOWED_HOSTS = ['*']
 
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:3000",  # Example: React frontend
+    "http://localhost:3000",
+]
+
 # Application definition
 
 INSTALLED_APPS = [
+    "corsheaders",
     "rest_framework",
     "paymaster",
     "django.contrib.admin",
@@ -48,6 +55,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
